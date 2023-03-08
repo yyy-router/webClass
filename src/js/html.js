a903c7c2173b5c71dfa1035236801b009bf18cd2
hqy <1804384725@qq.com>
2023-03-08 16:41:55 +0800

    first

---
+++ src/js/html.js
@@ -0,0 +1,32 @@
+const htmlStr = {
+   str :  `  <h4>手机商城</h4>
+<ul>
+    <li>手机名称</li>
+    <ol type="I">
+        <li>华为</li>
+        <ol type="1">
+            <li>Mate50Pro</li>
+            <li>Mate40Pro</li>
+            <li>nova10</li>
+        </ol>
+        <li>苹果</li>
+        <ol type="1">
+            <li>iPhone14 Pro Max</li>
+            <li>iPhone14</li>
+            <li>iPhone13</li>
+        </ol>
+        <li>小米</li>
+        <ol type="1">
+            <li>小米12Pro</li>
+            <li>小米13</li>
+            <li>小米12S</li>
+        </ol>
+        <li>三星</li>
+        <ol type="1">
+            <li>三星Galaxy S23</li>
+            <li>三星Galaxy S23 Ultra</li>
+            <li>三星Galaxy S22</li>
+        </ol>
+    </ol>`,
+    lang:'html'
+}
---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+const historyMenu = document.getElementById('history');
+const list = document.getElementById("menuList");
+const code = document.getElementById("code");
+let y1, y2, x1, x2;
+historyMenu.addEventListener("mouseover", (e) => {
+    y1 = e.clientY;
+    x1 = e.clientX;
+    list.style.display = "block";
+});
+historyMenu.addEventListener("mouseout", (e) => {
+    y2 = e.clientY;
+    x2 = e.clientX;
+    if (y2 < y1 || Math.abs(x2 - x1) > 40) {
+        list.style.display = "none";
+    }
+});
+list.addEventListener("mouseleave", () => {
+    list.style.display = "none";
+})
+// 代码展示部分
+const res = hljs.highlight(htmlStr.str,{
+    language:htmlStr.lang
+})
+code.className = `hljs language-${htmlStr.lang}`
+code.innerHTML = res.value;
+console.log('res',res)
